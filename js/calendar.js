f9c91495bf9c5f4617a26a2fdce4da13f506a867
pepperpapa <zhongxin2506@outlook.com>
2016-06-27 23:04:49 +0800

    finished ui of calendar month view

--- js/calendar.js
+++
@@ -1,7 +0,0 @@
-// 根据浏览器窗口变化实时控制calendar显示高度始终占据满屏，不出现滚动条
-var $calendar_area = $(".scroll-bar");
-$(window).on("resize", function(event) {
-  var height = $(window).height() - $("header").height()
-                             - $(".calendar-header").height();
-  $calendar_area.attr("height", height);
-}).trigger("resize");
