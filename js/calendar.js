ccb2751502beff85b41709122a71de6eef91ad9c
pepperpapa <zhongxin2506@outlook.com>
2016-07-10 01:12:34 +0800

    实现点击预定会议链接弹出预定会议室弹出框，弹出框的位置根据浏览器视口参数进行判断保证任何情况均能完整显示

--- js/calendar.js
+++ js/calendar.js
@@ -62,4 +62,31 @@ $(function() {
     $(this).parent().parent().removeClass("active");
   });
 
+  // 周视图、月视图某日扩展下点击预定会议室链接弹出预定会议室弹出框，采用事件委托方式
+  var $pop_over = $(".pop-over");
+  $(".calendar-content").on("click", ".link-book-meeting", function(e) {
+    $pop_over.addClass("is-shown");
+    // 如果以鼠标的x,y坐标为起点分别向右向下增加弹出的宽度和高度后超出了浏览器视口的范围
+    // 则需要适当调整弹出框的位置以使弹出框能够完整显示
+    if (e.clientX + $pop_over.width() > window.innerWidth) {
+      $pop_over.css("left", "auto");   // left参数设为默认值， right参数优先
+      $pop_over.css("right", 10 + "px");
+    } else {
+      $pop_over.css("right", "auto");
+      $pop_over.css("left", e.clientX + "px");
+    }
+
+    if (e.clientY + $pop_over.height() > window.innerHeight) {
+      $pop_over.css("top", "auto");  // top参数设为默认值， bottom参数优先
+      $pop_over.css("bottom", 10 + "px");
+    } else {
+      $pop_over.css("bottom", "auto");
+      $pop_over.css("top", e.clientY + "px");
+    }
+  });
+
+  // 关闭按钮关闭预定会议室弹出框
+  $(".pop-over .close-btn").on("click", function() {
+    $(".pop-over").removeClass("is-shown");
+  });
 });
