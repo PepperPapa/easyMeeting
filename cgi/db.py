f7424e143b8db822d5987165761a10569145c77d
pepperpapa <zhongxin2506@outlook.com>
2016-08-07 00:31:46 +0800

    test: don't create table if it already existed

--- cgi/db.py
+++ cgi/db.py
@@ -6,12 +6,20 @@ import sqlite3
 
 def dbtest():
     conn = sqlite3.connect("/home/zx/projects/easyMeeting/cgi/test.db")
-    conn.execute("""CREATE TABLE test
+    cursor = conn.cursor()
+
+    # get all table name in database
+    cursor.execute("SELECT NAME FROM sqlite_master WHERE TYPE='table'")
+    table_list = [name[0] for name in cursor.fetchall()]
+    
+    if not 'test' in table_list:
+        cursor.execute("""CREATE TABLE test
                  (ID INT PRIMARY KEY NOT NULL,
                   NAME TEXT NOT NULL);""")
-    conn.execute("""INSERT INTO test (ID, NAME) VALUES (001, "zx")""")
-    conn.commit()
-    cursor = conn.execute("SELECT * FROM test")
+        cursor.execute("""INSERT INTO test (ID, NAME) VALUES (001, "zx")""")
+        conn.commit()
+
+    cursor.execute("SELECT * FROM test")
     user =  cursor.fetchone()
     conn.close()
     return user
