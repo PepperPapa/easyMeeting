c69b03133662dd5e52b171964786c115fc6ff71e
pepperpapa <zhongxin2506@outlook.com>
2016-08-09 23:43:22 +0800

    in sigup page, user can create simple user and save to database

--- cgi/app.py
+++ cgi/app.py
@@ -2,14 +2,36 @@
 #!/usr/bin/python3
 # ubuntu16.04LTS
 
-from cgi import db
+if __name__ == '__main__':
+    import db
+else:
+    from cgi import db
+
+def handleSignup(environ):
+    query_string = environ['QUERY_STRING']
+    user_info = [item.split('=')[1] for item in query_string.split('&')]
+    new_user = db.insertNewUser(user_info[0], user_info[1], user_info[2])
+    if new_user:
+        return "sucess, name is %s" % new_user[0]
+    else:
+        return "user already exist!"
+
+def showEnviron(environ):
+    html = "<table>\n"		
+    for k, v in environ.items():		
+        html += "<tr><td>{}</td><td>{}</td></tr>\n".format(k, v)
+    html += "</table>\n"
+    return html
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
 
-    user = db.dbtest()
-    
-    html = "<h1>{}</h1><p>{}</p>".format(user[0], user[1])
+    url = environ['PATH_INFO']
+    if url == "/signup":
+        html = handleSignup(environ)
+        html += showEnviron(environ)
 
     return [html.encode("utf-8")]
 
+if __name__ == '__main__':
+    print(handleSignup({'QUERY_STRING': 'username=zx123&password=1234&re-password=1234'}))
--- cgi/db.py
+++ cgi/db.py
@@ -4,26 +4,40 @@
 
 import sqlite3
 
-def dbtest():
-    conn = sqlite3.connect("/home/zx/projects/easyMeeting/cgi/test.db")
-    cursor = conn.cursor()
-
+def connectDatabase():
+    conn = sqlite3.connect("/home/zx/projects/easyMeeting/cgi/easymeeting.db")
+    return (conn, conn.cursor())
+    
+def createUserTable():
+    conn, cursor = connectDatabase()
+    
     # get all table name in database
     cursor.execute("SELECT NAME FROM sqlite_master WHERE TYPE='table'")
     table_list = [name[0] for name in cursor.fetchall()]
     
-    if not 'test' in table_list:
-        cursor.execute("""CREATE TABLE test
-                 (ID INT PRIMARY KEY NOT NULL,
-                  NAME TEXT NOT NULL);""")
-        cursor.execute("""INSERT INTO test (ID, NAME) VALUES (001, "zx")""")
+    if not 'users' in table_list:
+        cursor.execute("""CREATE TABLE users
+                 (NAME TEXT PRIMARY KEY NOT NULL,
+                  PASSWORD TEXT NOT NULL);""")
+    return (conn, cursor)
+
+
+def insertNewUser(name, pwd, repeate_pwd):
+    # 如果users不存在则首先创建表users
+    conn, cursor = createUserTable()
+
+    # TODO: 用户名格式和密码格式校验
+    # TODO: 密码的加密处理
+    # 检查name是否存在，不存在才能插入值
+    cursor.execute("SELECT NAME FROM users WHERE NAME='{}'".format(name))
+    if (not cursor.fetchall()):
+        cursor.execute("""INSERT INTO users (NAME, PASSWORD)
+                       VALUES ('{}', '{}')""".format(name, pwd))
         conn.commit()
-
-    cursor.execute("SELECT * FROM test")
-    user =  cursor.fetchone()
-    conn.close()
-    return user
-
+        cursor.execute("SELECT * FROM users WHERE NAME='{}'".format(name))
+        user = cursor.fetchone()
+        conn.close()
+        return user
 
 if __name__ == '__main__':
-    print(dbtest())
+    print(insertNewUser("zx", "1234", "1234"))
